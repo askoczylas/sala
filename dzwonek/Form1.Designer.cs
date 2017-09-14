e68bef02f8e2ab9f161cdb76f786c18edbca7760
skoczylas <askoczylas@inveo.com.pl>
2017-09-14 22:35:24 +0200

    Add project files.

---
+++ dzwonek/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(593, 348);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 454);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ dzwonek/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int show = 1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Text = "ddd";
+            ShowText("1", new Font("Tahoma", 10, FontStyle.Regular), Color.Black);
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker2.RunWorkerAsync();
+        }
+
+       
+        private void  GetMulticastFrame()
+        {
+   //Configuration
+            var interfaceIp = IPAddress.Parse("192.168.1.143");
+            var interfaceEndPoint = new IPEndPoint(interfaceIp, 3671);
+            var multicastIp = IPAddress.Parse("224.0.23.12");
+            var multicastEndPoint = new IPEndPoint(multicastIp, 3671);
+
+            //initialize the socket
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.ExclusiveAddressUse = false;
+            socket.MulticastLoopback = false;
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
+            MulticastOption option = new MulticastOption(multicastEndPoint.Address, interfaceIp);
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, option);
+
+            //bind on a network interface
+            socket.Bind(interfaceEndPoint);
+
+            //initialize args for sending packet on the multicast channel
+            var sockArgs = new SocketAsyncEventArgs();
+            sockArgs.RemoteEndPoint = multicastEndPoint;
+            sockArgs.SetBuffer(new byte[1234], 0, 1234);
+
+            //send an empty packet of size 1234 every 3 seconds
+            while (true)
+            {
+               // socket.SendToAsync(sockArgs);
+                byte[] b = new byte[1024];
+
+                socket.Receive (b);
+                show = 1;
+               
+                Thread.Sleep(3000);
+            }
+            //byte[] b = new byte[1024];
+            //s.Receive(b);
+            //string str = System.Text.Encoding.ASCII.GetString(b, 0, b.Length);
+            //Console.WriteLine(str.Trim());
+
+        }
+
+        public void ShowText(string text, Font font, Color col)
+        {
+
+            Brush brush = new SolidBrush(col);
+
+
+
+            // Create a bitmap and draw text on it
+
+            Bitmap bitmap = new Bitmap(16, 16);
+            Font m_font = new Font("Tahoma",15,FontStyle.Regular);
+
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            graphics.DrawString(text, m_font, brush, 0, 0);
+
+
+
+            // Convert the bitmap with text to an Icon
+
+            IntPtr hIcon = bitmap.GetHicon();
+
+            Icon icon = Icon.FromHandle(hIcon);
+
+            notifyIcon1.Icon = icon;
+
+        }
+        
+        private void GetFrame()
+        {
+            string sourceUrl = "http://192.168.0.56/jpg/image.jpg";
+            byte[] buffer = new byte[1280 * 800];
+            int read, total = 0;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(sourceUrl);
+            req.Credentials = new NetworkCredential("admin", "1234");
+            
+            WebResponse resp = req.GetResponse();
+
+            Stream stream = resp.GetResponseStream();
+
+            while ((read=stream.Read(buffer,total,1000)) !=0)
+            {
+                total+=read;
+            }
+            Bitmap bmp = (Bitmap)Bitmap.FromStream(new MemoryStream(buffer, 0, total));
+
+            pictureBox1.Image = bmp;
+
+        
+        
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+
+              //  GetMulticastFrame();
+
+             if (show==1) GetFrame();
+
+                Thread.Sleep(500);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            {
+                show = 0;
+                Hide();
+            };
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            GetMulticastFrame();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = show.ToString();
+            timer1.Enabled = false;
+            if (show == 1)
+            {
+                Show();
+                this.WindowState = FormWindowState.Normal;
+            };
+
+            timer1.Enabled = true;
+
+        }
+    
+    
+    }
+}
